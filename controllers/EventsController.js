dd855a3b8764362c43de359612a53a14999f5718
wrona96 <TheCrow96@protonmail.com>
2019-04-24 16:34:02 +0200

    EventsController add support for PUT and DELETE

--- controllers/EventsController.js
+++ controllers/EventsController.js
@@ -1,10 +1,13 @@
 const Events = require('../models/Events')
+const options = {
+  runValidators: true
+}
 
 module.exports = {
 
   get: () => {
     return new Promise((resolve, reject) => {
-      Events.find({},{ _id: 0 }).then(data => {
+      Events.find().then(data => {
         resolve(data)
       }).catch(err => {
         reject(err)
@@ -16,6 +19,35 @@ module.exports = {
     return new Promise((resolve, reject) => {
       Events.create(params).then(data => {
         resolve('Succesfull registed.')
+      }).catch(err => {
+        reject(err)
+      })
+    })
+  },
+
+  put: (body) => {
+    return new Promise((resolve, reject) => {
+      id = body._id
+      delete body._id
+      Events.findByIdAndUpdate(id, body, options).then(data => {
+        if (data == null) {
+          throw new Error("Wrong ID.")
+        }
+        resolve("Event data updated.")
+      }).catch(err => {
+        reject(err)
+      })
+    })
+  },
+
+  delete: (body) => {
+    return new Promise((resolve, reject) => {
+      id = body._id
+      Events.findByIdAndRemove(id).then(data => {
+        if (data == null) {
+          throw new Error("Wrong ID.")
+        }
+        resolve("Event removed.")
       }).catch(err => {
         reject(err)
       })
