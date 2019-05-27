20990a6e7bf2ba05c32a887d121c20048ece98a8
wrona96 <TheCrow96@protonmail.com>
2019-05-27 12:35:05 +0200

    add cookie-parser

--- secure.js
+++ secure.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const timeout = require('connect-timeout');
 const favicon = require('serve-favicon');
 
 //Init
 const secure = express();
-secure.use(helmet())
+secure.use(helmet());
 secure.disable('x-powered-by');
 secure.use(timeout('15s'));
 secure.set('view engine', 'pug');
 secure.use(express.static(__dirname + '/public'));
 secure.use(favicon(__dirname + '/public/images/favicon.ico'));
-secure.use(bodyParser.json({ type: 'application/json' }))
+secure.use(bodyParser.json({ type: 'application/json' }));
+secure.use(cookieParser());
 
 //redirect all to https
 /*secure.use ((req, res, next) => {
