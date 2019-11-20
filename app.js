f4bda0ba1f9db660511b529678692bf529ac2e0c
wrona96 <TheCrow96@protonmail.com>
2019-11-20 18:17:15 +0100

    remove auth from deleted user

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const app = require('./secure')
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken')
+const User = require('./models/Users.js')
 const options = {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -18,10 +19,11 @@ const options = {
   family: 4 // Use IPv4, skip trying IPv6
 };
 
-mongoose.connect(process.env.DB || 'mongodb://localhost/tetmet',options).then(
-  () => {   console.error('DB Connected');},
-  err => {  console.error('DB not Connected'); }
-);
+mongoose.connect(process.env.DB || 'mongodb://localhost/tetmet', options).then(() => {
+  console.error('DB Connected');
+}, err => {
+  console.error('DB not Connected');
+});
 
 //Set Routers
 const index = require('./routes/index')
@@ -35,22 +37,31 @@ function authorize(req, res, next) {
   jwt.verify(req.headers['x-access-token'], process.env.JWT || 'TESTOWANIE', function(err, decoded) {
     if (err) {
       res.status(401).render('401');
-    }else{
-      req.headers.userId = decoded._id;
-      next();
+    } else {
+      User.findOne({'_id': decoded._id}).then(data => {
+        if (data == 0) {
+          res.status(401).render('401');
+        } else {
+          req.headers.userLogin = data.nickname;
+          req.headers.userId = decoded._id;
+          next();
+        }
+      })
     }
   });
 }
 
 //Error 404 handling
 app.use((req, res) => {
-   res.status(404).render('404', {title: '404: Page Not Found'});
+  res.status(404).render('404', {title: '404: Page Not Found'});
 });
 
 //Error 500 handling
 app.use((error, req, res, next) => {
-   res.status(500).render('500', {title:'500: Internal Server Error', error: error});
+  res.status(500).render('500', {
+    title: '500: Internal Server Error',
+    error: error
+  });
 });
-
 
 module.exports = app
