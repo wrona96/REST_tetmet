21c31b8befe54a3cc6271d8c8f75bb2e9cba05d6
wrona96 <TheCrow96@protonmail.com>
2019-12-06 10:34:40 +0100

    fix DeprecationWarning

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const options = {
   useUnifiedTopology: true,
   useFindAndModify: false,
   autoIndex: false, // Don't build indexes
-  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-  reconnectInterval: 500, // Reconnect every 500ms
   poolSize: 10, // Maintain up to 10 socket connections
   // If not connected, return errors immediately rather than waiting for reconnect
   bufferMaxEntries: 0,
